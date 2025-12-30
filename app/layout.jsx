37e49f4f5dab1ffffb75429cf42df7dcf1281178
Roney Gajjar <hh.kc.tech@proton.me>
2025-12-30 14:16:40 +0530

    updated page.tsx and layout.tsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,4 @@
 import '../styles/globals.css';
-import { Footer } from '../components/footer';
-import { Header } from '../components/header';
-
 export const metadata = {
     title: {
         template: '%s | Netlify',
@@ -15,13 +12,11 @@ export default function RootLayout({ children }) {
             <head>
                 <link rel="icon" href="/favicon.svg" sizes="any" />
             </head>
-            <body className="antialiased text-white bg-blue-900">
-                <div className="flex flex-col min-h-screen px-6 bg-noise sm:px-12">
-                    <div className="flex flex-col w-full max-w-5xl mx-auto grow">
-                        <Header />
-                        <main className="grow">{children}</main>
-                        <Footer />
-                    </div>
+            <body>
+                <div>
+                    <div>
+                        <main>{children}</main>
+                   </div>
                 </div>
             </body>
         </html>

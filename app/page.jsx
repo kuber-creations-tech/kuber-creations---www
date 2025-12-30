565743e096cbf10ad9ed30b6f6bc0a86690e6e5a
Roney Gajjar <roneygajjar121@gmail.com>
2025-12-30 16:37:53 +0530

    removed unnecesary files

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,9 @@
 export default function Page() {
     return (
         <div className="h-screen w-screen text-center flex flex-col justify-center gap-12 sm:gap-16">
-            Welcome to Kuber Creations            
+            <h1 className="text-6xl">
+                Welcome to Kuber Creations
+            </h1> 
         </div>
     );
 }

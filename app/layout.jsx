85480f3406f5823dbbce1dcf03d55bd505fe46d6
Roney Gajjar <roneygajjar121@gmail.com>
2025-12-31 15:39:28 +0530

    feat: add Google Tag Manager scripts for enhanced tracking

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,13 +10,22 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <head>
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-0N4QKTX6L7"></script>
+                <script> {`    
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-0N4QKTX6L7');
+                `}
+                </script>
                 <link rel="icon" href="/favicon.svg" sizes="any" />
             </head>
+
             <body>
                 <div>
                     <div>
                         <main>{children}</main>
-                   </div>
+                    </div>
                 </div>
             </body>
         </html>

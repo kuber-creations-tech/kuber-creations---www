baa6c2434f8df39dbe76f1da83a6942c891cce30
Roney Gajjar <roneygajjar121@gmail.com>
2026-01-03 14:35:42 +0530

    feat: update layout to use GoogleAnalytics component and simplify structure; upgrade Next.js and dependencies

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,26 +1,13 @@
+import { GoogleAnalytics } from '@next/third-parties/google'
 import '../styles/globals.css';
 
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <head>
-                <script async src="https://www.googletagmanager.com/gtag/js?id=G-0N4QKTX6L7"></script>
-                <script> {`    
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', 'G-0N4QKTX6L7');
-                `}
-                </script>
-            </head>
-
             <body>
-                <div>
-                    <div>
-                        <main>{children}</main>
-                    </div>
-                </div>
+                <main>{children}</main>
             </body>
+            <GoogleAnalytics gaId="G-0N4QKTX6L7" />
         </html>
     );
 }
